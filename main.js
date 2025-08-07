5585a4510e391cfd46082667ed14e158f0ab0420
link28032000 <jbataller0@gmail.com>
2025-08-07 15:47:40 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+let modalOuter = document.getElementById("modalOuter");
+
+const openModal = () => {
+   
+        modalOuter.classList.remove("closeModal");
+};
+
+const closeModal = () => {
+    
+        modalOuter.classList.add("closeModal");
+   
+};
